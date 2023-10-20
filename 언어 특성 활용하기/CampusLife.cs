afbcfe7ab3d90f7106b3a3b748235b2d2d073b3b
BIT <BIT@DESKTOP-MVM6023>
2023-10-20 14:18:01 +0900

    초기 커밋.

---
+++ 언어 특성 활용하기/CampusLife.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 언어_특성_활용하기
+{
+    internal class CampusLife
+    {
+    }
+}
---
+++ 언어 특성 활용하기/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 언어_특성_활용하기
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Init();
+            Run();
+        }
+
+        private static void Run()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void Init()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
